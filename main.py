2be4bf9689df28e431fe7559551bbc47c89a224f
Valentino Amato <valentinoamatoroberts@gmail.com>
2022-09-29 09:28:57 -0300

    Added comments/ fixed typo

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ from dotenv import load_dotenv
 from discord.ext import commands
 import random
 
-load_dotenv()
+load_dotenv()                 #Loads the virtual enviroment
 
 intents = discord.Intents.default()
 intents.message_content = True
-
-
 
 rbot = commands.Bot(intents=intents)
 @rbot.event
@@ -17,31 +15,21 @@ async def on_ready():
     print(f'We have logged in as {rbot.user}')
 
 
-
-
-@rbot.event
-async def on_message(message):
-    if message.author == rbot.user:
-        return
-    
-    if message.content == '$hello':
-        await message.channel.send('hello!!!')
-
-@rbot.slash_command(guild_ids=[1017094174316699769,1011807843164373092,965657683153793205,884926708355051581])        #Flips a coin
+@rbot.slash_command()        #Flips a coin
 async def coinflip(ctx: discord.ApplicationContext):
     """Flips a coin."""
     c = ["HEADS!","TAILS!"]
     await ctx.respond("Flipping a coin!")
     await ctx.send(f"Result: {random.choice(c)}")
 
-@rbot.slash_command(guild_ids=[1017094174316699769,1011807843164373092,965657683153793205,884926708355051581])        #Flips a coin
+@rbot.slash_command()        #Spins a roulette
 async def spin(ctx: discord.ApplicationContext,  number: discord.Option(str,name= "number",description="Amount of numers of the roulette.",required=True)):
     """Spins a roulette."""
     await ctx.respond("Spinning the roulette!")
     await ctx.send(f"Numbers: {number}")
     await ctx.send(f"Result: {random.randint(1,int(number))}")
 
-@rbot.slash_command(guild_ids=[1017094174316699769,1011807843164373092,965657683153793205,884926708355051581])        #Flips a coin
+@rbot.slash_command()        #Displays information
 async def help(ctx: discord.ApplicationContext):
     """Displays information."""
     embed=discord.Embed(title="Random Bot",url="https://github.com/valentinoamato/Random-bot", description="Commands:",color=0xff5733)
@@ -51,19 +39,19 @@ async def help(ctx: discord.ApplicationContext):
     embed.add_field(name="/24",value="Shows four random numbers to play a game called twenty four.\nBasically the idea of this game is to take the four numbers  and make math operations between them to get to 24.\n\nE.g given the numbers 12,6,5 and 2: (6-5)\*2\*12=24.",inline=False)
     await ctx.respond(embed=embed)
 
-class Menu(discord.ui.View):
+class Menu(discord.ui.View):         #Creates a menu with two buttons
     def __init__(self):
         super().__init__()
         self.value = None
 
-    @discord.ui.button(label="New Cards.",style=discord.ButtonStyle.blurple)
+    @discord.ui.button(label="New Cards.",style=discord.ButtonStyle.blurple)  #First button: gives new numbers
     async def menu1(self,button: discord.ui.Button, interaction: discord.Interaction):
         embed = discord.Embed(color=discord.Color.random())
         embed.add_field(name=f"{random.randint(1,12)}                    {random.randint(1,12)}",value="----------------",inline=False)
         embed.add_field(name=f"{random.randint(1,12)}                    {random.randint(1,12)}",value="----------------",inline=False)
-        await interaction.response.edit_message(embed=embed,content="This are your new cards:")
+        await interaction.response.edit_message(embed=embed,content="These are your new cards:")
 
-    @discord.ui.button(label="End Game.",style=discord.ButtonStyle.grey)
+    @discord.ui.button(label="End Game.",style=discord.ButtonStyle.grey)      #Second button: finishes the manu
     async def menu2(self,button: discord.ui.Button, interaction: discord.Interaction):
         embed = discord.Embed(color=discord.Color.random())
         embed.add_field(name="Thanks for Playing!!",value="\n<:wave:1024734969207205970><:wave:1024734969207205970><:wave:1024734969207205970>")
@@ -71,13 +59,13 @@ class Menu(discord.ui.View):
         self.value = False
         self.stop()
 
-@rbot.slash_command(guild_ids=[1017094174316699769,1011807843164373092,965657683153793205,884926708355051581],name="24")
+@rbot.slash_command(name="24")      #Runs the menu
 async def menu(ctx):
-    """Shows four random spanish cards to play 24"""
+    """Shows four random spanish cards to play 24."""
     view = Menu()
     embed = discord.Embed(color=discord.Color.random())
     embed.add_field(name=f"{random.randint(1,12)}                    {random.randint(1,12)}",value="----------------",inline=False)
     embed.add_field(name=f"{random.randint(1,12)}                    {random.randint(1,12)}",value="----------------",inline=False)
-    await ctx.respond("This are your cards:",view=view,embed=embed)
+    await ctx.respond("These are your cards:",view=view,embed=embed)
 
-rbot.run(os.getenv('token'))
+rbot.run(os.getenv('token'))  #Runs the bot with the token in the .env
